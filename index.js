d25fcc58f4c6782e73781f5ffed0d1876b34b65d
Adam Mannino <amannino92@gmail.com>
2023-12-14 18:40:54 -0500

    Completed Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph){cats.push("Ralph")
+return cats;}
+function destructivelyPrependCat(Bob){cats.unshift("Bob")
+return cats;}
+function destructivelyRemoveLastCat(){cats.pop()
+return cats;}
+function destructivelyRemoveFirstCat(){cats.shift()
+return cats;}
+function appendCat(Broom){var cats2= [...cats,"Broom"];
+return cats2;}
+function prependCat(Arnold){var cats2= ["Arnold", ...cats,];
+return cats2;}
+function removeLastCat(){var cats2= cats.slice(0,cats.length-1);
+return cats2;}
+function removeFirstCat(){var cats2= cats.slice(1);
+return cats2;}  
